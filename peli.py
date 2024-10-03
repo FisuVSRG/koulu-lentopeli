94c75c045f7688b48812515133af505090df488d
Jarkko <jaksu.kokkonen@gmail.com>
2024-10-03 10:04:52 +0300

    testiversio

--- peli.py
+++ peli.py
@@ -9,10 +9,33 @@ yhteys = mysql.connector.connect(
          autocommit=True
          )
 
-sql = f"SELECT * from goal"
-print(sql)
-kursori = yhteys.cursor()
-kursori.execute(sql)
-tulos = kursori.fetchall()
-for rivi in tulos:
-    print(rivi)
+tila = 0
+
+# Pelin päävalikko
+while tila == 0:
+    print("Tervetuloa pelaamaan!")
+    print("1) Aloita peli")
+    print("2) Tulostaulukko")
+    print("3) Sulje ohjelma")
+    aloitus = input("Valitse haluatko tehdä uuden käyttäjän vai jatkaa vanhalta. \n")
+
+# Tilan valinta ja pelin aloitus
+    if aloitus == "1":
+        tila = 1
+        tunnus = input("Syötä käyttäjätunnus: ")
+        tallenna_kayttajatunnus(tunnus)
+
+# Tulostaulukko
+    elif aloitus == "2":
+        tila = 1
+# Lisää tähän funktio joka hakee korkeimmat tulokset järjestyksessä ja niiden käyttäjätunnukset.
+
+# Ohjelman lopettaminen
+    elif aloitus == "3":
+        tila = 1
+        break
+
+# Virheellinen syöte
+    else:
+        print("Syöte on virheellinen. Anna syöte uudelleen.")
+
